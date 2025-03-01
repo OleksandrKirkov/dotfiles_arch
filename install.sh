4b2ff4962c84bdc05f758bed0291469f13ffef94
OleksandrKirkov <kirkovsasha009@gmail.com>
2025-03-01 02:31:13 +0200

    feat(hyprland): configure hyprland

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@ set -e
 sudo pacman -Syu --noconfirm
 
 sudo pacman -S --noconfirm neovim python-pynvim git nodejs \
-    npm curl zsh nvidia nvidia-utils nvidia-settings nvidia-prime
+    npm curl zsh nvidia nvidia-utils nvidia-settings nvidia-prime \
+    hyprland xdg-desktop-portal-hyprland wlroots wayland xorg-xwayland \
+    polkit-kde-agent dunst rofi kitty waybar wofi foot thunar hyprpaper
 
 sudo pacman -S --noconfirm hyprland noto-fonts noto-fonts-cjk noto-fonts-emoji
 
